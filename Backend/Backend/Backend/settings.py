d00b5288c38851fa1d81812aa19d79ac79a04884
Dionis <callmehdion@gmail.com>
2023-09-03 11:53:33 +0800

    re-do setup

--- Backend/Backend/Backend/settings.py
+++ Backend/Backend/Backend/settings.py
@@ -46,11 +46,11 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.spotify',
-    'allauth.socialaccount.providers.google',
     'dj_rest_auth',
     'rest_framework.authtoken',
     'corsheaders',
     'django_extensions',
+
 ]
 
 MIDDLEWARE = [
--- Backend/Backend/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('permissions/', views.GetRoles.as_view(), name='get-roles')
-]
