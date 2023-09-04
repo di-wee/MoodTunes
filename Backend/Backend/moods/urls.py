182bdd9f97363a309588d6e3932825c598fad57f
Dionis <callmehdion@gmail.com>
2023-09-04 13:21:01 +0800

    playlist serializer created

--- Backend/Backend/moods/urls.py
+++ Backend/Backend/moods/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.GetMoods.as_view(), name='get-moods'),
-    path('submoods/<int:mood_id>/', views.GetSubMoods.as_view(), name='get-submoods-from-moods')
+    path('submoods/<int:mood_id>/', views.GetSubMoods.as_view(), name='get-submoods-from-moods'),
+    path('', views.CreateMoods.as_view(), name='create-mood'),
+    path('submooods/', views.CreateSubMoods.as_view(), name='create-submoods')m
+
 ]
--- Backend/Backend/moods/views.py
+++ Backend/Backend/moods/views.py
@@ -12,6 +12,22 @@ class GetMoods(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+class CreateMoods(APIView):
+    def post(self, request):
+
+        # de-serializing JSON into data for python
+        serializer = MoodSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Unable to create mood.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
 class GetSubMoods(APIView):
     def get(self, request, mood_id=None):
 
@@ -22,4 +38,17 @@ class GetSubMoods(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response({'error': 'Mood ID not provided or does not exists'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class CreateSubMoods(APIView):
+    def post(self, request):
+        serializer = SubMoodSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Unable to create sub-mood.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
 
