1832bad9dc626c2a0076c71934a4c47bcb7e938f
Dionis <callmehdion@gmail.com>
2023-09-05 18:22:26 +0800

    log in page somewhat done

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,29 +4,37 @@ import UserDashboard from './pages/UserDashboard';
 import UserPlaylist from './pages/UserPlaylist';
 import AdminDashboard from './pages/AdminDashboard';
 import Main from './pages/Main';
-
+import { ThemeProvider, createTheme } from '@mui/material';
+import { indigo } from '@mui/material/colors';
+const theme = createTheme({
+	typography: {
+		fontFamily: 'Roboto Condensed, sans-serif',
+	},
+});
 function App() {
 	return (
-		<div>
+		<div style={{ backgroundColor: indigo[100] }}>
 			<main>
-				<Routes>
-					<Route
-						path='/'
-						element={<Main />}
-					/>
-					<Route
-						path='/user/dashboard'
-						element={<UserDashboard />}
-					/>
-					<Route
-						path='/user/playlist'
-						element={<UserPlaylist />}
-					/>
-					<Route
-						path='/admin/dashboard'
-						element={<AdminDashboard />}
-					/>
-				</Routes>
+				<ThemeProvider theme={theme}>
+					<Routes>
+						<Route
+							path='/'
+							element={<Main />}
+						/>
+						<Route
+							path='/user/dashboard'
+							element={<UserDashboard />}
+						/>
+						<Route
+							path='/user/playlist'
+							element={<UserPlaylist />}
+						/>
+						<Route
+							path='/admin/dashboard'
+							element={<AdminDashboard />}
+						/>
+					</Routes>
+				</ThemeProvider>
 			</main>
 		</div>
 	);
--- React/src/components/Login.jsx
+++ React/src/components/Login.jsx
@@ -1,7 +1,84 @@
+import { Box, Button, Container, Typography } from '@mui/material';
+import { blue, indigo } from '@mui/material/colors';
 import React from 'react';
-
+import { Icon } from '@iconify/react';
+import { Spotify } from 'mdi-material-ui';
+import { LoginOutlined } from '@mui/icons-material';
 const Login = () => {
-	return <div></div>;
+	return (
+		<div
+			style={{
+				height: '100vh',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				position: 'relative',
+			}}>
+			<Container maxWidth='xs'>
+				<Box
+					sx={{
+						bgcolor: blue[100],
+						height: '11vh',
+						width: '30%',
+						borderRadius: '15%',
+						opacity: '75%',
+						position: 'relative',
+						top: '10.5rem',
+						left: '35%',
+						zIndex: 1,
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}>
+					<Typography
+						sx={{ color: indigo[800] }}
+						variant='h5'>
+						LOG IN
+					</Typography>
+				</Box>
+				<Box
+					sx={{
+						bgcolor: indigo[600],
+						height: '35vh',
+						width: '100%',
+						borderRadius: '10%',
+						opacity: '85%',
+						position: 'relative',
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}>
+					<Typography
+						variant='h5'
+						sx={{ color: blue[50], marginBottom: '3rem' }}>
+						Welcome Back
+					</Typography>
+					<Button
+						endIcon={<Spotify></Spotify>}
+						variant='contained'
+						sx={{
+							textTransform: 'none',
+							width: '60%',
+							height: '20%',
+							marginTop: '0rem',
+						}}>
+						Log in with Spotify
+					</Button>
+					<Button
+						endIcon={<LoginOutlined></LoginOutlined>}
+						sx={{
+							textTransform: 'none',
+							width: '60%',
+							color: blue[50],
+							marginTop: '1rem',
+						}}>
+						Sign up with Spotify
+					</Button>
+				</Box>
+			</Container>
+		</div>
+	);
 };
 
 export default Login;
