4c129304288b00387d4400310d7f8c09e916b408
Dionis <callmehdion@gmail.com>
2023-09-05 10:01:55 +0800

    playback urls done

--- Backend/Backend/playbacks/urls.py
+++ Backend/Backend/playbacks/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search/', views.SpotifySongSearch.as_view(), name='spotify_search'),
+    path('play/', views.Play.as_view(), name='play'),
+    path('pause/', views.Pause.as_view(), name='pause'),
+    path('previous_track/', views.Previous.as_view(), name='previous-track'),
+    path('next_track/', views.Next.as_view(), name='next-track'),
 ]
