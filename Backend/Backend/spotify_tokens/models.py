5d123d45937155e64ea2d4fba03d99ce96a387a8
Dionis <callmehdion@gmail.com>
2023-09-07 15:47:17 +0800

    authentication via temp token, getting token& jwt

--- Backend/Backend/spotify_tokens/models.py
+++
@@ -1,9 +0,0 @@
-from datetime import timedelta
-from django.db import models
-from users.models import CustomUser
-from django.utils import timezone
-
-
-class SpotifyToken(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
-    access_token = models.CharField(max_length=255)
--- Backend/Backend/spotify_tokens/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('extract_token/', views.ExtractCode.as_view(), name='extract-token'),
-    path('get_jwt/', views.GetJwtToken.as_view(), name='get-jwt'),
-]
