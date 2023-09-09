0dbe1448cb3f9751ee47350b87ffca90381c8258
Dionis <callmehdion@gmail.com>
2023-09-09 19:25:37 +0800

    fixed spotify oauth bug

---
+++ Backend/Backend/users/adapters.py
@@ -0,0 +1,10 @@
+
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+
+
+class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
+    def save_user(self, request, sociallogin, form=None):
+        user = super().save_user(request, sociallogin, form)
+        user.save()
+        return user
